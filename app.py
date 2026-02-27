3d6d006932abee518652ecee9f439b322ea320e1
Ryan Corazza Alvarenga <ryancorazzaalvarenga@gmail.com>
2026-02-27 13:54:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def home():
     # Aqui você pode passar variáveis como o título da HQ ou sinopse
     dados_hq = {
         "titulo": "Cacau em: Conhecendo a UFABC",
-        "autor": "Ryan Corazza",
-        "descricao": "Conheça o Cacau e a Betty e embarque nesta aventura muito divertida onde descobrem como a Universidade Federal do ABC funciona!"
+        "autor": "Ryan Corazza e Pâmela Campos",
+        "descricao": "Conheça o Cacau e a Betty e descubra como a Universidade Federal do ABC (UFABC) funciona nesta aventura muito divertida!"
     }
     return render_template('index.html', hq=dados_hq)
 
